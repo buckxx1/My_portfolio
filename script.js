1beb13e9965f786511f83f3915d0bc16cdba8b0f
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-03-05 15:27:57 -0500

    third commit of the day

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("resume-btn-1").addEventListener("click", function() {
+
+    //myWindow = window.open ("https://drive.google.com/file/d/1sgPLQ5yDxwEVicPRmaIqAyISaemXmNVD/view")
+}
+//
