5d77b21adaca3ed8e9a1f9604bf05a4df25afe65
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-03-05 15:33:15 -0500

    button fix attempt

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.getElementById("resume-btn-1").addEventListener("click", function() {
-
-    //myWindow = window.open ("https://drive.google.com/file/d/1sgPLQ5yDxwEVicPRmaIqAyISaemXmNVD/view")
+document.getElementById("resume-btn-1").addEventListener("click", function openWin() {
+    myWindow = window.open("https://drive.google.com/file/d/1sgPLQ5yDxwEVicPRmaIqAyISaemXmNVD/view")
+    
 }
 //
